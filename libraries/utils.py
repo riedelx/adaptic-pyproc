69f437f117bf2b2b647127f8fcc4e145f9eaf9db
riedelx <kamil.k.riedel@gmail.com>
2022-01-11 14:52:52 +0000

    update: utils.py

--- libraries/utils.py
+++ libraries/utils.py
@@ -110,7 +110,7 @@ def create_ASTR(positive, negative = []):
         ASTR.extend((k1,x1,k2,x2,k3))
     return ASTR
 
-def ASTR_plot(lst, ult_p, ult_n, title, x_label, y_label, scaleX = 1, scaleY = 1,xlim=(None,None),plotSide='both'):
+def ASTR_plot(lst, ult_p, ult_n, title='astr', x_label='', y_label='', scaleX = 1, scaleY = 1,xlim=(None,None),plotSide='both',returnData = False):
     #lst = lst[1:]
     positive = lst[1:6]
     negative = lst[6:11]
@@ -129,14 +129,27 @@ def ASTR_plot(lst, ult_p, ult_n, title, x_label, y_label, scaleX = 1, scaleY = 1
             else:
                 x3 = x2
             y3 = y2 + k3 * (x3 - x2)
-            ASTR_pos = np.array([[0,0],[x1,y1],[x2,y2],[x3,y3]])
+            # check if the line crosses 0
+            if y3/y2 < 0:
+                y3, y4 = 0, 0
+                x4 = x2-y2/k3
+            else:
+                x4, y4 = x3, y3
+            ASTR_pos = np.array([[0,0],[x1,y1],[x2,y2],[x4,y4],[x3,y3]])
         else:
             if np.abs(ult_n) >= np.abs(x2):
                 x3 = ult_n
             else:
                 x3 = x2
             y3 = y2 + k3 * (x3 - x2)
-            ASTR_neg = np.flip(np.array([[0,0],[x1,y1],[x2,y2],[x3,y3]]), axis = 0)
+            # check if the line crosses 0
+            if y3/y2 < 0:
+                y3 = 0
+                y4 = 0
+                x4 = x2-y2/k3
+            else:
+                x4, y4 = x3, y3
+            ASTR_neg = np.flip(np.array([[0,0],[x1,y1],[x2,y2],[x4,y4],[x3,y3]]), axis = 0)
         positive_bool = False
     if plotSide=='both':
         ASTR_xy = np.vstack((ASTR_neg,ASTR_pos))
@@ -144,14 +157,16 @@ def ASTR_plot(lst, ult_p, ult_n, title, x_label, y_label, scaleX = 1, scaleY = 1
         ASTR_xy = ASTR_pos
     elif plotSide=='negative':
         ASTR_xy = ASTR_neg
-    fig, ax = plt.subplots()
-    ax.grid(which='major', linestyle=':', linewidth='0.5', color='black')
-    plt.plot(ASTR_xy[:,0] * scaleX,ASTR_xy[:,1] * scaleY)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.xlim(xlim)
-    plt.title(title)
-    plt.show()
+    if returnData: return ASTR_xy
+    else:
+        fig, ax = plt.subplots()
+        ax.grid(which='major', linestyle=':', linewidth='0.5', color='black')
+        plt.plot(ASTR_xy[:,0] * scaleX,ASTR_xy[:,1] * scaleY)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.xlim(xlim)
+        plt.title(title)
+        plt.show()
 
 def astr_gap(gapP,gapN,S1=2.00E+09,S2=1.00E+09,plotting=True,xlim=[20,-20],title='astr gap curve',xlabel='displacement [mm]',ylabel='force [kN]', scaleX = 1, scaleY = 1):
     # S1, S2 are the stiffnesses after closing the gap, S1 > S2
@@ -196,117 +211,109 @@ def centroidX(points,discr=100):
     return moment/area
 
 # #=====================================================#
-# # from PyFEA
-# def matchSign(a,b):
-#     if b >= 0:
-#         return a
-#     else:
-#         return -a
-#
-# def round_sig(x, sig=2):
-#     if x ==0: return 0
-#     else: return round(x, sig-int(floor(log10(abs(x))))-1)
-#
-# def findExactPoint(curve1, coordinate,limY=True, multiple=False): # also in postproc
-#     if limY:
-#         curve2=np.array([[-9E99,np.inf],[coordinate,coordinate]])
-#     else:
-#         curve2=np.array([[coordinate,coordinate],[-9E99,np.inf]])
-#     tupl = (intersection(curve1[0], curve1[1], curve2[0], curve2[1]))
-#     try:
-#         return [float(tupl[0]),float(tupl[1])]
-#     except:
-#         if multiple:
-#             return (tupl[0]),(tupl[1])
-#         else:
-#             return [float(tupl[0][0]),float(tupl[1][0])]
-#
-# # Intersection of two curves
-# """
-# Sukhbinder
-# 5 April 2017
-# Based on:
-# """
-# def _rect_inter_inner(x1,x2):
-#     n1=x1.shape[0]-1
-#     n2=x2.shape[0]-1
-#     X1=np.c_[x1[:-1],x1[1:]]
-#     X2=np.c_[x2[:-1],x2[1:]]
-#     S1=np.tile(X1.min(axis=1),(n2,1)).T
-#     S2=np.tile(X2.max(axis=1),(n1,1))
-#     S3=np.tile(X1.max(axis=1),(n2,1)).T
-#     S4=np.tile(X2.min(axis=1),(n1,1))
-#     return S1,S2,S3,S4
-#
-# def _rectangle_intersection_(x1,y1,x2,y2):
-#     S1,S2,S3,S4=_rect_inter_inner(x1,x2)
-#     S5,S6,S7,S8=_rect_inter_inner(y1,y2)
-#
-#     C1=np.less_equal(S1,S2)
-#     C2=np.greater_equal(S3,S4)
-#     C3=np.less_equal(S5,S6)
-#     C4=np.greater_equal(S7,S8)
-#
-#     ii,jj=np.nonzero(C1 & C2 & C3 & C4)
-#     return ii,jj
-#
-# def intersection(x1,y1,x2,y2): # also in postproc
-#     """
-# INTERSECTIONS Intersections of curves.
-#    Computes the (x,y) locations where two curves intersect.  The curves
-#    can be broken with NaNs or have vertical segments.
-# usage:
-# x,y=intersection(x1,y1,x2,y2)
-#     Example:
-#     a, b = 1, 2
-#     phi = np.linspace(3, 10, 100)
-#     x1 = a*phi - b*np.sin(phi)
-#     y1 = a - b*np.cos(phi)
-#     x2=phi
-#     y2=np.sin(phi)+2
-#     x,y=intersection(x1,y1,x2,y2)
-#     plt.plot(x1,y1,c='r')
-#     plt.plot(x2,y2,c='g')
-#     plt.plot(x,y,'*k')
-#     plt.show()
-#     """
-#     ii,jj=_rectangle_intersection_(x1,y1,x2,y2)
-#     n=len(ii)
-#
-#     dxy1=np.diff(np.c_[x1,y1],axis=0)
-#     dxy2=np.diff(np.c_[x2,y2],axis=0)
-#
-#     T=np.zeros((4,n))
-#     AA=np.zeros((4,4,n))
-#     AA[0:2,2,:]=-1
-#     AA[2:4,3,:]=-1
-#     AA[0::2,0,:]=dxy1[ii,:].T
-#     AA[1::2,1,:]=dxy2[jj,:].T
-#
-#     BB=np.zeros((4,n))
-#     BB[0,:]=-x1[ii].ravel()
-#     BB[1,:]=-x2[jj].ravel()
-#     BB[2,:]=-y1[ii].ravel()
-#     BB[3,:]=-y2[jj].ravel()
-#
-#     for i in range(n):
-#         try:
-#             T[:,i]=np.linalg.solve(AA[:,:,i],BB[:,i])
-#         except:
-#             T[:,i]=np.NaN
-#
-#     in_range= (T[0,:] >=0) & (T[1,:] >=0) & (T[0,:] <=1) & (T[1,:] <=1)
-#
-#     xy0=T[2:,in_range]
-#     xy0=xy0.T
-#     return xy0[:,0],xy0[:,1]
-#
-# def maxAbs(a,b):
-#     if np.abs(a) >= np.abs(b):
-#         return a
-#     else:
-#         return b
-# # from PyFEA
+# Intersection of two curves
+"""
+Sukhbinder
+5 April 2017
+Based on:
+"""
+def _rect_inter_inner(x1,x2):
+    n1=x1.shape[0]-1
+    n2=x2.shape[0]-1
+    X1=np.c_[x1[:-1],x1[1:]]
+    X2=np.c_[x2[:-1],x2[1:]]
+    S1=np.tile(X1.min(axis=1),(n2,1)).T
+    S2=np.tile(X2.max(axis=1),(n1,1))
+    S3=np.tile(X1.max(axis=1),(n2,1)).T
+    S4=np.tile(X2.min(axis=1),(n1,1))
+    return S1,S2,S3,S4
+
+def _rectangle_intersection_(x1,y1,x2,y2):
+    S1,S2,S3,S4=_rect_inter_inner(x1,x2)
+    S5,S6,S7,S8=_rect_inter_inner(y1,y2)
+
+    C1=np.less_equal(S1,S2)
+    C2=np.greater_equal(S3,S4)
+    C3=np.less_equal(S5,S6)
+    C4=np.greater_equal(S7,S8)
+
+    ii,jj=np.nonzero(C1 & C2 & C3 & C4)
+    return ii,jj
+
+def name_reset(df1,df2,prefix="nst"):
+    df1=df1.drop(["name"], axis=1)
+    df=pd.concat([df1,df2])
+    df=df.drop_duplicates(inplace=False,subset='ID')
+    dfName = utils.name_gen(prefix,df)
+    df.insert(0, "name", dfName, True)
+    return df.reset_index(drop=True)
+
+def intersection(x1,y1,x2,y2):
+    """
+INTERSECTIONS Intersections of curves.
+   Computes the (x,y) locations where two curves intersect.  The curves
+   can be broken with NaNs or have vertical segments.
+usage:
+x,y=intersection(x1,y1,x2,y2)
+    Example:
+    a, b = 1, 2
+    phi = np.linspace(3, 10, 100)
+    x1 = a*phi - b*np.sin(phi)
+    y1 = a - b*np.cos(phi)
+    x2=phi
+    y2=np.sin(phi)+2
+    x,y=intersection(x1,y1,x2,y2)
+    plt.plot(x1,y1,c='r')
+    plt.plot(x2,y2,c='g')
+    plt.plot(x,y,'*k')
+    plt.show()
+    """
+    x1,y1,x2,y2 = np.array(x1),np.array(y1),np.array(x2),np.array(y2)
+    ii,jj=_rectangle_intersection_(x1,y1,x2,y2)
+    n=len(ii)
+
+    dxy1=np.diff(np.c_[x1,y1],axis=0)
+    dxy2=np.diff(np.c_[x2,y2],axis=0)
+
+    T=np.zeros((4,n))
+    AA=np.zeros((4,4,n))
+    AA[0:2,2,:]=-1
+    AA[2:4,3,:]=-1
+    AA[0::2,0,:]=dxy1[ii,:].T
+    AA[1::2,1,:]=dxy2[jj,:].T
+
+    BB=np.zeros((4,n))
+    BB[0,:]=-x1[ii].ravel()
+    BB[1,:]=-x2[jj].ravel()
+    BB[2,:]=-y1[ii].ravel()
+    BB[3,:]=-y2[jj].ravel()
+
+    for i in range(n):
+        try:
+            T[:,i]=np.linalg.solve(AA[:,:,i],BB[:,i])
+        except:
+            T[:,i]=np.NaN
+
+
+    in_range= (T[0,:] >=0) & (T[1,:] >=0) & (T[0,:] <=1) & (T[1,:] <=1)
+
+    xy0=T[2:,in_range]
+    xy0=xy0.T
+    return xy0[:,0],xy0[:,1]
+
+def findExactPoint(curve1, coordinate,limY=True, multiple=False):
+    if limY:
+        curve2=np.array([[-9E99,np.inf],[coordinate,coordinate]])
+    else:
+        curve2=np.array([[coordinate,coordinate],[-9E99,np.inf]])
+    tupl = (intersection(curve1[0], curve1[1], curve2[0], curve2[1]))
+    try:
+        return [float(tupl[0]),float(tupl[1])]
+    except:
+        if multiple:
+            return (tupl[0]),(tupl[1])
+        else:
+            return [float(tupl[0][0]),float(tupl[1][0])]
 # #=====================================================#
 
 class param2adap:
